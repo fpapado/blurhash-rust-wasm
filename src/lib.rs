de6375a5ad5298302a3cb62befd1651d79fbd2c0
fpapado <fotis@futurice.com>
2019-07-25 23:30:54 +0300

    Get decode working

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,153 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-pub fn decode(blur_hash: String) {}
+
+use std::f64::consts::PI;
+
+#[derive(PartialEq, Debug)]
+pub enum Error {
+    LengthInvalid,
+    LengthMismatch,
+}
+
+pub fn decode(blur_hash: &str, width: usize, height: usize) -> Result<Vec<u8>, Error> {
+    if blur_hash.len() < 6 {
+        return Err(Error::LengthInvalid);
+    }
+
+    // 1. Number of components
+    // For a BlurHash with nx components along the X axis and ny components
+    // along the Y axis, this is equal to (nx - 1) + (ny - 1) * 9.
+    let size_flag = decode_base83_string(blur_hash.get(0..1).unwrap());
+
+    let num_y = (size_flag / 9) + 1;
+    let num_x = (size_flag % 9) + 1;
+
+    // Validate that the number of digits is what we expect:
+    // 1 (size flag) + 1 (maximum value) + 4 (average colour) + (num_x - num_y - 1) components * 2 digits each
+    let expected_digits = 4 + 2 * num_x * num_y;
+
+    if blur_hash.len() != expected_digits {
+        return Err(Error::LengthMismatch);
+    }
+
+    // 2. Maximum AC component value, 1 digit.
+    // All AC components are scaled by this value.
+    // It represents a floating-point value of (max + 1) / 166.
+    let quantised_maximum_value = decode_base83_string(blur_hash.get(1..2).unwrap());
+    let maximum_value = ((quantised_maximum_value + 1) as f64) / 166f64;
+
+    let mut colours: Vec<[f64; 3]> = Vec::new();
+
+    for i in 0..(num_x * num_y) {
+        if i == 0 {
+            // 3. Average colour, 4 digits.
+            let value = decode_base83_string(blur_hash.get(2..6).unwrap());
+            colours.push(decode_dc(value));
+        } else {
+            // 4. AC components, 2 digits each, nx * ny - 1 components in total.
+            let value = decode_base83_string(blur_hash.get((4 + i * 2)..(4 + i * 2 + 2)).unwrap());
+            // TODO: Adjustable 'punch'
+            colours.push(decode_ac(value, maximum_value * 1.0));
+        }
+    }
+
+    // Now, construct the image
+    let bytes_per_row = width * 3;
+
+    let mut pixels = vec![0; bytes_per_row * height];
+
+    for y in 0..height {
+        for x in 0..width {
+            let mut r = 0f64;
+            let mut g = 0f64;
+            let mut b = 0f64;
+
+            for j in 0..num_y {
+                for i in 0..num_x {
+                    let basis = f64::cos(PI * (x as f64) * (i as f64) / (width as f64))
+                        * f64::cos(PI * (y as f64) * (j as f64) / (height as f64));
+                    let colour = colours[i + j * num_x];
+                    r += colour[0] * basis;
+                    g += colour[1] * basis;
+                    b += colour[2] * basis;
+                }
+            }
+
+            let int_r = linear_to_srgb(r);
+            let int_g = linear_to_srgb(g);
+            let int_b = linear_to_srgb(b);
+
+            pixels[3 * x + 0 + y * bytes_per_row] = int_r;
+            pixels[3 * x + 1 + y * bytes_per_row] = int_g;
+            pixels[3 * x + 2 + y * bytes_per_row] = int_b;
+        }
+    }
+
+    Ok(pixels)
+}
+
+fn decode_dc(value: usize) -> [f64; 3] {
+    let int_r = value >> 16;
+    let int_g = (value >> 8) & 255;
+    let int_b = value & 255;
+    [
+        srgb_to_linear(int_r),
+        srgb_to_linear(int_g),
+        srgb_to_linear(int_b),
+    ]
+}
+
+fn decode_ac(value: usize, maximum_value: f64) -> [f64; 3] {
+    let quant_r = f64::floor((value / (19 * 19)) as f64);
+    let quant_g = f64::floor(((value / 19) as f64) % 19f64);
+    let quant_b = (value as f64) % 19f64;
+
+    let rgb = [
+        sign_pow((quant_r - 9f64) / 9f64, 2f64) * maximum_value,
+        sign_pow((quant_g - 9f64) / 9f64, 2f64) * maximum_value,
+        sign_pow((quant_b - 9f64) / 9f64, 2f64) * maximum_value,
+    ];
+    rgb
+}
+
+fn sign_pow(value: f64, exp: f64) -> f64 {
+    get_sign(value) * f64::powf(f64::abs(value), exp)
+}
+
+fn get_sign(n: f64) -> f64 {
+    if n < 0f64 {
+        -1f64
+    } else {
+        1f64
+    }
+}
+
+fn linear_to_srgb(value: f64) -> u8 {
+    let v = f64::max(0f64, f64::min(1f64, value));
+    if v <= 0.0031308 {
+        return (v * 12.92 * 255f64 + 0.5) as u8;
+    } else {
+        return ((1.055 * f64::powf(v, 1f64 / 2.4) - 0.055) * 255f64 + 0.5) as u8;
+    }
+}
+
+fn srgb_to_linear(value: usize) -> f64 {
+    let v = (value as f64) / 255f64;
+    if v <= 0.04045 {
+        return v / 12.92;
+    } else {
+        return ((v + 0.055) / 1.055).powf(2.4);
+    }
+}
 
 const ENCODE_CHARACTERS: &str =
     "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz#$%*+,-.:;=?@[]^_{|}~";
 
-fn decode_base83_string(string: String) -> usize {
+fn decode_base83_string(string: &str) -> usize {
     let mut value: usize = 0;
+
     for character in string.chars() {
-        match ENCODE_CHARACTERS.chars().position(|c| c == character) {
+        match ENCODE_CHARACTERS.find(character) {
             Some(digit) => value = value * 83 + digit,
 
             None => (),
@@ -23,12 +162,16 @@ mod tests {
 
     #[test]
     fn it_decodes_size_flag() {
-        assert_eq!(21, decode_base83_string(String::from("L")));
-        assert_eq!(0, decode_base83_string(String::from("0")));
+        assert_eq!(21, decode_base83_string("L"));
+        assert_eq!(0, decode_base83_string("0"));
     }
     #[test]
     fn decodes_size_0_out_of_range() {
-        let res = decode_base83_string(String::from("/"));
-        assert_eq!(0, res, "Did not expect to decode size for input out of range (expected 0), but got {}", res);
+        let res = decode_base83_string("/");
+        assert_eq!(
+            0, res,
+            "Did not expect to decode size for input out of range (expected 0), but got {}",
+            res
+        );
     }
 }
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,6 +1,27 @@
-// use blurhash;
+use blurhash::decode;
+use image;
 
 #[test]
-fn test_work() {
-    assert_eq!(2,2);
+fn err_if_hash_length_less_than_6() {
+    assert_eq!(Err(blurhash::Error::LengthInvalid), decode("L", 40, 30));
 }
+
+#[test]
+fn decodes_ok() {
+    // From the online demo
+    let res = decode("LKO2?U%2Tw=w]~RBVZRi};RPxuwH", 40, 30);
+
+    // From a known encode/decode
+    let expected = image::open("decode-test-in.png").unwrap().to_rgb();
+
+    match res {
+        Ok(img) => {
+            assert_eq!(expected.to_vec(), img);
+        }
+
+        Err(_err) => assert!(false),
+    }
+}
+
+// TODO: encodes_ok (open file, encode, match)
+// TODO: Round trip
