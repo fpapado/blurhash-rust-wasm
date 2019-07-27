afee0c53d70158ef027d4745715888ad34ed2294
fpapado <fotis@futurice.com>
2019-07-27 14:56:42 +0300

    Initial demo done

--- demo/App.js
+++ demo/App.js
@@ -2,34 +2,89 @@ import * as blurhash from "blurhash-wasm";
 
 // Using react/preact in this demo mostly for convenience and habit.
 // You can of course render blurhashes however you want!
-import { h } from "preact";
+import { h, Component } from "preact";
+import { useRef, useEffect } from "preact/hooks";
 
 export function App(props) {
   const { images } = props;
   return (
-    <main>
+    <main className="pa3 mw7 center">
       <h1>Rust Wasm Blurhash</h1>
+      <p className="measure">
+        A Rust and WebAssembly implementation of the{" "}
+        <a href="https://blurha.sh">blurhash algorithm</a>.
+      </p>
+      <p className="measure">
+        You can seamlessly use it in the browser through JS imports and bundling.
+      </p>
       {images.map(image => (
-        <CustomImage image={image} key={image.id} />
+        <div>
+          <div className="w4">
+            <CustomImage image={image} key={image.id} />
+          </div>
+          <ImageCredit credit={image.credit} />
+        </div>
       ))}
     </main>
   );
 }
 
 function CustomImage(props) {
-  const { id, src, alt, blurhash, ratio, credit } = props.image;
-  console.log(props)
+  const { src, alt, hash, ratio } = props.image;
 
-  // Returned as Uint8Array | undefined
-  // const pixels = blurhash.decode("LKO2?U%2Tw=w]~RBVZRi};RPxuwH", 32, 32);
+  // Hardcoded width/height for the decode and canvas
+  // We keep these low and let the UI scale them up
+  const WIDTH = 32; // pixels
+  const HEIGHT = 32; // pixels
 
-  // if (pixels) {
-  //   const asClamped = new Uint8ClampedArray(pixels);
-  //   const imageData = new ImageData(asClamped, 32, 32);
+  const canvasRef = useRef(null);
 
-  //   const canvasEl = document.getElementById("image-canvas");
-  //   const ctx = canvasEl.getContext("2d");
-  //   ctx.putImageData(imageData, 0, 0);
-  // }
-  return <img src={src} alt={alt} />;
+  // When the component mounts, set the image placeholder
+  useEffect(() => {
+    // Decode the hash into pixels
+    // Returned as Uint8Array | undefined
+    const pixels = blurhash.decode(hash, WIDTH, HEIGHT);
+    if (pixels) {
+      // Set the pixels to the canvas
+      const asClamped = new Uint8ClampedArray(pixels);
+      const imageData = new ImageData(asClamped, WIDTH, HEIGHT);
+
+      const canvasEl = canvasRef.current;
+
+      if (canvasEl) {
+        const ctx = canvasEl.getContext("2d");
+        ctx.putImageData(imageData, 0, 0);
+      }
+    }
+  }, [hash]);
+
+  // Cycle the opacity of the image on and off
+
+  // Layout notes:
+  // canvas goes below the img
+  // both canvas and img have a fixed aspect-ratio placeholder
+  // to preserve the layout size before either has loaded.
+  return (
+    <div>
+      <div className={`aspect-ratio aspect-ratio--${ratio}`}>
+        <canvas
+          ref={canvasRef}
+          width={WIDTH}
+          height={HEIGHT}
+          className="aspect-ratio--object"
+        />
+        <img src={src} alt={alt} cassName="aspect-ratio--object" />
+      </div>
+    </div>
+  );
+}
+
+function ImageCredit(props) {
+  const { username, displayName } = props.credit;
+  return (
+    <p className="measure">
+      Credit to <a href={`https://unsplash.com/@${username}`}>{displayName}</a>{" "}
+      on <a href="https://unsplash.com">Unsplash</a>
+    </p>
+  );
 }
