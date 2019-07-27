5c24ca85993784f4ed57ad9aab6290e0edcebd59
fpapado <fotis@futurice.com>
2019-07-27 13:28:30 +0300

    Build script text

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # The initial build
-# Set wee_alloc as a smaller allocator
+# Set wee_alloc as a smaller allocator.
 # We do it here instead of Cargo.toml, to keep
-# the default allocator in the native Rust library build
+# the default allocator in the native Rust library build.
 wasm-pack build -- --features wee_alloc
 
 echo "Initial size"
 
 wc -c pkg/blurhash_wasm_bg.wasm
 
-# Optimize for size
-# You might need to install wasm-opt from binaryen
+# Optimize wasm
+# NOTE: Again, setting -Os did not decrease the size,
+# so might as well optimise for speed.
+# You might need to install wasm-opt from binaryen:
 # https://github.com/WebAssembly/binaryen/releases
-wasm-opt -O3 -o pkg/blurhash_wasm_bg.wasm pkg/blurhash_wasm_bg.wasm
+wasm-opt pkg/blurhash_wasm_bg.wasm -O3 -o pkg/blurhash_wasm_bg.wasm
 
 echo "Size after wasm-opt"
 wc -c pkg/blurhash_wasm_bg.wasm
