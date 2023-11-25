d4cc07d43b849b609ac5a901423b78ae595de6f2
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2023-11-25 23:58:43 +0200

    Add note on opt

--- build-wasm.sh
+++ build-wasm.sh
@@ -8,10 +8,11 @@ echo "Initial size"
 wc -c pkg/blurhash_wasm_bg.wasm
 
 # Optimize wasm
-# NOTE: Again, setting -Os did not decrease the size,
-# so might as well optimise for speed.
 # You might need to install wasm-opt from binaryen:
-# https://github.com/WebAssembly/binaryen/releases
+# via npm: https://www.npmjs.com/package/binaryen
+# or via the binary releases: https://github.com/WebAssembly/binaryen/releases
+# NOTE: Tried different options and -O3 gave the smallest code size
+# did not decrease the size, so might as well optimise for speed.
 wasm-opt pkg/blurhash_wasm_bg.wasm -O3 -o pkg/blurhash_wasm_bg.wasm
 
 echo "Size after wasm-opt"
