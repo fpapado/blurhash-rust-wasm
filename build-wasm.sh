44c861fcedd0ad4f9893aa77dfd70a9d76a7fe05
Fotis Papadogeorgopoulos <fotis.papadogeorgopoulos@wolt.com>
2023-11-26 14:54:15 +0200

    Build both bundler and node targets

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
-# The initial build
-wasm-pack build
+# web (bundler) build
+wasm-pack build --target bundler --release --out-dir pkg
 
-echo "Initial size"
-
+echo "[bundler target] Initial size"
 wc -c pkg/blurhash_wasm_bg.wasm
 
-# Optimize wasm
-# You might need to install wasm-opt from binaryen:
-# via npm: https://www.npmjs.com/package/binaryen
-# or via the binary releases: https://github.com/WebAssembly/binaryen/releases
-# NOTE: Tried different options and -O3 gave the smallest code size
-# did not decrease the size, so might as well optimise for speed.
-wasm-opt pkg/blurhash_wasm_bg.wasm -O3 -o pkg/blurhash_wasm_bg.wasm
+echo "[bundler target] Size after gzip"
+gzip -9 < pkg/blurhash_wasm_bg.wasm | wc -c
 
-echo "Size after wasm-opt"
+# node build
+wasm-pack build --target nodejs --release --out-dir pkg-node
+
+echo "[node target] Initial size"
 wc -c pkg/blurhash_wasm_bg.wasm
 
-echo "Size after gzip"
+echo "[node target] Size after gzip"
 gzip -9 < pkg/blurhash_wasm_bg.wasm | wc -c
